ff30bb6b8a68c63f59a94c135d23ddc905b3aaf6
tushal024 <tushalshekh@gmail.com>
2024-10-27 18:41:01 +0530

    add

--- js/product.js
+++ js/product.js
@@ -1,22 +1,126 @@
 
-    fetch(`http://localhost:3000/product`)
-    .then((r)=>{
+fetch(`http://localhost:3000/product`)
+    .then((r) => {
         return r.json();
     })
-    .then((res)=>{
-        document.getElementById("sec-1").innerHTML=view(res)
-        // console.log(res);    
+    .then((res) => {
         
+        let shoes_arr=shoes1(res)
+        
+        console.log(res);
+        document.getElementById(`sec-1`).innerHTML = view(res)
+
+        let zid= fil(res)
+        console.log(zid.length);
+
+// shoes.......................................................................................................
+
+let shoes=document.getElementById("shoes")
+
+shoes.addEventListener('change',function(){
+    if(this.checked){
+        console.log("yes");
+    //    console.log(shoes1(res));
+        console.log(shoes_arr);
+
+        shoes_arr.map((el)=>{
+            console.log(el.price);
+            document.getElementById("sec-1").innerHTML=`<h1>${el.price}</h1>`
+            
+        })
+        
+  
+    }
+    else{
+        console.log("no");
+        
+    }
+
+})
+
+
+
+
+
+        // 400 up...................................................................................................
+        let a = document.getElementById("up")
+        a.addEventListener('change', function () {
+            
+            if (this.checked) {
+                console.log("check.........!");
+                // document.getElementById("sec-1").innerHTML = fil(res)
+                console.log(zid[6]);
+                for(let x=6;x<zid.length;x++){
+                    
+                    document.getElementById(`sec-${x}`).innerHTML =` <div id="pr1" class="w-[300px] ms-[5px] h-[400px] relative p-[15px]  rounded-[10px] mb-[10px]" style="border: 1px solid rgba(0, 0, 0, 0.226);">
+                    <i class="fa-regular fa-heart absolute right-[9%] text-[#DFDCD9] top-[5%] text-[24px]"></i>
+                  <div id="pr_img" class="h-[70%] w-[95%] " >  <a href="single_pr.html?id=${zid[x].id}"> <img src="${zid[x].img}" class=" rounded-[10px] ms-[2.5%]  h-[100%] w-[100%]"> </div>
+        
+                  <div id="pr_text" class="h-[25%] pt-[20px]" >
+                      <p class="mb-[0px] text-[15px]">${zid[x].des}</p>
+                      <span class="font-[600] text-[18px]">₹ ${zid[x].price}      </span>
+        
+                      <span id="line" class="text-[14px]">${zid[x].n_price}</span>
+                      <span   class="w-[auto] ms-[10px] mt-[7px] mb-[7px] text-[14px] font-[500] inline-block  ps-[8px] pe-[8px] "  style="background-color: #d5f8e6;">  ${zid[x].discount} % Off</span>
+        
+                  </div>
+              </div></a>`
+        
+                }     
+            }
+            else {
+                console.log("Not Check............!");
+                window.location.reload()
+             
+
+            }
+        })
+
 
     })
-    .catch((er)=>{
+    .catch((er) => {
         console.log(er);
     })
 
 
-    function view(arr){
-        return arr.map((el)=>{
-            return `
+
+
+
+    function shoes1(arr){
+
+        let sh=[]
+        sh= arr.filter((el)=>{
+
+            if(el.type=="shose"){
+                return sh
+            }
+        })
+
+        return sh;
+        }
+
+
+function fil(res) {
+    let ar = [];
+    ar = res.filter((el) => {
+        if (el.price > 400) {
+            console.log(el);
+        
+               return  ar
+        }
+    })
+    console.log(ar);
+
+    return ar;
+    
+}
+
+// .....................................................................................................
+
+
+function view(arr) {
+    return arr.map((el) => {
+        return `
               <div id="pr1" class="w-[32%] h-[400px] relative p-[15px] rounded-[10px] mb-[10px]" style="border: 1px solid rgba(0, 0, 0, 0.226);">
               <i class="fa-regular fa-heart absolute right-[9%] text-[#DFDCD9] top-[5%] text-[24px]"></i>
             <div id="pr_img" class="h-[70%] w-[95%] " >  <a href="single_pr.html?id=${el.id}"> <img src="${el.img}" class=" rounded-[10px] ms-[2.5%]  h-[100%] w-[100%]"> </div>
@@ -32,8 +136,8 @@
         </div></a>
 
             `
-        }).join("")
-    }
+    }).join("")
+}
 
 
 
--- js/single_pr.js
+++ js/single_pr.js
@@ -34,15 +34,72 @@ function fun() {
 }
 
 function view(el) {
-    return ` <div id="pr1" class="w-[32%] h-[500px]">
-            <div id="pr_img" class="h-[80%] w-[100%]" style="border: 1px solid black;">  <img src="${el.img}" class="h-[100%] w-[100%]"> </div>
+    return `
     
-            <div id="pr_text" class="h-[20%]" style="border: 1px solid black;">
-                <p class="mb-[10px]">${el.des}</p>
-                <p>₹ ${el.price}  <span id="line">${el.n_price}</span>    <span class="ms-[10px]">  ${el.discount} % Off</span></p>
-                <button id="add">ADD TO CART</button>
+        <div id="product" class="w-[80%] h-[782px] ml-[120px] mt-[10px]" style="display: flex; justify-content: space-evenly;">
+            <div id="1" class="w-[7%] h-[300px]" style="display: flex; flex-direction: column; justify-content: space-evenly;">
+                <div id="sub-1" class="w-[100%] h-[80px]" style=" border-radius: 10px;"></div>
+                <div id="sub-1-2" class="w-[100%] h-[80px]" style="border-radius: 10px;"></div>
+                <div id="sub-1-3" class="w-[100%] h-[80px]" style=" border-radius: 10px;"></div>
             </div>
-        </div>`
+            <div id="2" class="w-[35%] h-[500px]" style="border-radius: 5px;">
+                    <div id="sub-2" class="w-[100%] h-[500px]" style="border-radius: 10px;">
+                    <img src="${el.img}" class="w-[100%] h-[500px]">
+                    </div>
+                <button id="add"  >Add to Cart</button>
+            </div>
+            <div id="3" class="w-[50%] h-[782px]" style="">
+                    <div id="sub-3" class="w-[100%] h-[180px]" style="border: 1px solid rgb(205, 198, 198); border-top: none; border-left: none; border-right: none;">
+                     <p id="P2">BULLMER</p>
+                     <h1 id="H1">${el.des}</h1>
+                     <span id="S1"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i><i class="fa-regular fa-star"></i> <span id="S4">454</span> <span id="S5"><i class="fa-regular fa-heart"></i> &nbsp; <i class="fa-solid fa-share-nodes"></i></span></span>
+                     <h1 id="H2">₹ ${el.price}.00   <span id="S2"> ${el.discount}% off</span></h1>
+                     <p id="P3">M.R.P: <span id="S3">₹${el.n_price}.00</span> (Incl. of all taxes)</p>
+                    </div>
+                 <div id="sub-3-1" class="w-[100%] h-[120px]" style="border: 1px solid rgb(205, 198, 198); border-top: none; border-left: none; border-right: none;">
+                    <h1 id="H3">Colour</h1>
+                    <div id="sub-3-1/1" class="w-[180px] h-[50px]" style=" display: flex; justify-content: space-evenly; align-items: center;">
+                            <div id="ro1" class="w-[48px] h-[40px]" style="border: 1px solid black; border-radius: 10px; display: flex; justify-content: center; align-items: center;">
+                                    <div id="ro-1" class="w-[20px] h-[20px]" style="background-color: #f0d9b0; border-radius: 50%;"></div>
+                            </div>
+                            <div id="ro1" class="w-[48px] h-[40px]" style="border: 1px solid black; border-radius: 10px; display: flex; justify-content: center; align-items: center;">
+                                <div id="ro-2" class="w-[20px] h-[20px]" style="background-color: black; border-radius: 50%;"></div>
+                            </div>
+                            <div id="ro1" class="w-[48px] h-[40px]" style="border: 1px solid black; border-radius: 10px; display: flex; justify-content: center; align-items: center;">
+                                <div id="ro-3" class="w-[20px] h-[20px]" style="background-color: blue; border-radius: 50%;"></div>
+                            </div>
+                    </div>
+                 </div>
+                 <div id="sub-3-2" class="w-[100%] h-[100px]" style="border: 1px solid rgb(205, 198, 198); border-top: none; border-left: none; border-right: none;">
+                        <h1 id="H4">Size</h1>
+                        <div id="size" class="w-[200px] h-[50px]" style=" display: flex; justify-content: space-evenly;  align-items: center;">
+                            <div id="si1" class="w-[39px] h-[26px]" style="border: 1px solid black; border-radius: 20px; text-align: center;"><b>S</b></div>
+                            <div id="si1" class="w-[39px] h-[26px]" style="border: 1px solid black; border-radius: 20px; text-align: center;"><b>M</b></div>
+                            <div id="si1" class="w-[39px] h-[26px]" style="border: 1px solid black; border-radius: 20px; text-align: center;"><b>L</b></div>
+                            <div id="si1" class="w-[39px] h-[26px]" style="border: 1px solid black; border-radius: 20px; text-align: center;"><b>XL</b></div>
+                        </div>
+                 </div>
+                 <div id="sub-3-3" class="w-[100%] h-[170px]" style="border: 1px solid rgb(205, 198, 198); border-top: none; border-left: none; border-right: none;">
+                        <h1 id="H5">Offers (13)</h1>
+                        <div id="of" class="w-[380px] h-[70px]" style="display: flex; justify-content: space-evenly; align-items: center;">
+                       <div id="icon" class="w-[30px] h-[30px]" style=" display: flex; justify-content: center; align-items: center;"> 
+                        <i class="fa-solid fa-building-columns" id="icon1"></i></div>
+                        <div id="line"><p id="P4">BANK OFFERS</p>
+                        <P id="P5">5 accelerated reward points by SBI cards (1.25%), 10 accelerated reward.... <i class="fa-solid fa-angle-right" id="icon2"></i></P>
+                        <p id="P6">13 offer/s Available</p></div>
+                        </div>
+                        <button id="btn2">View All</button>
+                 </div>
+                 <div id="sub-3-4" class="w-[100%] h-[100px]" style="border: 1px solid rgb(205, 198, 198); border-top: none; border-left: none; border-right: none;">
+                        <h1 id="H6">Return Policy</h1>
+                        <p id="P7">Return and Refund - Please refer to the policy here.</p>
+                 </div>
+                 <div id="sub-3-4" class="w-[100%] h-[80px]" style="border: 1px solid rgb(205, 198, 198); border-top: none; border-left: none; border-right: none; border-bottom: none;">
+                        <p id="P8">Article ID: RVLCZZ8KAO <i class="fa-solid fa-paste" id="icon3"></i></p>    
+                 </div>
+            </div>
+        </div>    
+    `
 }
 
 fun()
