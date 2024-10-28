3e70bcc3030171dc6ccb9a0f907a2c72e4b5c552
HarshRaval31 <ravalh205@gmail.com>
2024-10-28 16:43:17 +0530

    Day3

---
+++ js/login.js
@@ -0,0 +1,70 @@
+var kk = true
+var ans = []
+document.querySelector("#login").addEventListener("submit", (h) => {
+    h.preventDefault();
+
+    var loginemail = document.getElementById("loginemail").value
+    var loginpass = document.getElementById("loginpassword").value
+
+    kk = true
+    document.querySelector("#loginpasstext").innerHTML = ""
+    document.querySelector("#loginemailtext").innerHTML = ""
+
+
+    if (loginemail.length == 0) {
+        document.querySelector("#loginemailtext").innerHTML =
+            "Plese Enter Valid Password";
+        kk = false;
+
+    }
+    if (loginpass.length < 8) {
+        document.querySelector("#loginpasstext").innerHTML = "Plese Enter Valid Password";
+        kk = false;
+
+    }
+
+    if(kk == true)
+    {
+        fetch(`https://boat-api-1.onrender.com/signupdata`)
+        .then((re)=>{
+            return re.json()
+        }).then((res)=>{
+            console.log(res);
+            getdata(res)
+        }).catch((err)=>{
+            console.log(err);
+        })
+    }
+   
+
+    if (kk == true) {
+        function getdata(arr){
+            ans = arr.filter((el)=>{
+                if(loginemail == el.email && loginpass == el.pass)
+                {
+                    return el
+                }
+             })
+            if (ans.length > 0) {
+                Swal.fire({
+                    title: "Log In Successful...!",
+                    text: "You clicked the button!",
+                    icon: "success"
+                });
+
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "Log In Failed...",
+                    text: "Something went wrong!",
+                    footer: '<a href="#">Why do I have this issue?</a>'
+                })
+            }
+        }
+
+
+        
+    }
+
+    
+})
